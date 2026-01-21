bc6c33f3058ce7c67a4f11cb372c10798936c8b4
TakundaMabukwa <mabukwatakunda@gmail.com>
2026-01-21 08:50:10 +0200

    pm2 cluuster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,12 @@ module.exports = {
   apps: [{
     name: 'video-server',
     script: './dist/index.js',
-    instances: 'max',
+    instances: 4,
     exec_mode: 'cluster',
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'production',
-      TCP_PORT: 7611,
-      UDP_PORT: 6611,
-      API_PORT: 3000
+      NODE_ENV: 'production'
     },
-    error_file: './logs/err.log',
-    out_file: './logs/out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
     merge_logs: true,
     autorestart: true,
